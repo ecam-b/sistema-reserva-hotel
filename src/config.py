ea9025e7507a1422cf66909f362a7269a9730d44
ecam-b <ec.angaritas@gmail.com>
2023-03-28 15:56:27 -0500

    Realizando las configuraciones de la conexión.

--- src/config.py
+++ src/config.py
@@ -1,1 +1,10 @@
-config.py
+from decople import config
+
+user = config("PGSQL_USER")
+password = config("PGSQL_PASSWORD")
+host = config("PGSQL_HOST")
+database = config("PGSQL_DATABASE")
+
+SECRET_KEY = config("SECRET_KEY")
+
+DATABASE_URI_CONNECTION = f"postgresql+psycopg2://{user}:{password}@{host}/{database}"
