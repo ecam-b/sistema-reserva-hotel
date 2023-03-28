a0b78abca940a355d65dc6ce6811d703f14c439a
ecam-b <ec.angaritas@gmail.com>
2023-03-28 16:02:07 -0500

    Configuración de la base de datos lista.

--- src/app.py
+++ src/app.py
@@ -1,1 +1,16 @@
-app.py
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+# database
+from database.db import db
+# config
+from config import SECRET_KEY, DATABASE_URI_CONNECTION
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = SECRET_KEY
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI_CONNECTION
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+SQLAlchemy(app)
+
+if __name__ == "__main__":
+	app.run(debug=True)
