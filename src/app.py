b91c2f64178a7f4ff05e4c44bd48a93066998ebd
ecam-b <ec.angaritas@gmail.com>
2023-03-29 08:56:00 -0500

    Se agregaron los blueprints, lanzando servidor para validar errores.

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ from database.db import db
 # config
 from config import SECRET_KEY, DATABASE_URI_CONNECTION
 # routes
-from routes import Typeuser, User
+from routes import Typeuser, User, Payment_method, Payment, Hotel, Type_room, Room, Reservation
 
 
 app = Flask(__name__)
@@ -27,4 +27,10 @@ if __name__ == "__main__":
 	# blueprints
 	app.register_blueprint(Typeuser.typeuser_bp, url_prefix="/typeuser")
 	app.register_blueprint(User.user_bp, url_prefix="/user")
+	app.register_blueprint(Payment_method.payment_method_bp, url_prefix="/payment_method")
+	app.register_blueprint(Payment.payment_bp, url_prefix="/payment")
+	app.register_blueprint(Hotel.hotel_bp, url_prefix="/hotel")
+	app.register_blueprint(Type_room.type_room_bp, url_prefix="/type_room")
+	app.register_blueprint(Room.room_bp, url_prefix="/room")
+	app.register_blueprint(Reservation.reservation_bp, url_prefix="/reservation")
 	app.run(debug=True)
